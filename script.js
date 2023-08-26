25331f6215927ffc3869685e911abb4cad991fc9
claudiahW <murungiclaud@gmail.com>
2023-08-27 01:21:06 +0300

    add a for loop to generate random values

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeColor(){
     let hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+
+    let hexcode = ''; //where the hexcode that we generate will go
+
+    for(let i=0; i < 6;i++){       //the for loop will generate the code for us //since i<6 it will loop five times
+
+
+    } 
 }
